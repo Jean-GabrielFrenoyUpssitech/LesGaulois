9fac86c632ae8d0f57b47849d2b6e7c569f71bf6
Jean-GabrielFrenoyUpssitech <jean-gabriel.frenoy@univ-tlse3.fr>
2025-01-09 11:52:34 +0100

    fini implementation Les Gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,26 @@
 package personnages;
 
 public class Gaulois {
+	private String nom;
+	private int force;
 
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	private String getNom() {
+		return nom;
+	}
+
+	public int getForce(int force) {
+		return force;
+	}
+	public void parler() {
+		System.out.println("Les gaulois" + nom + ": << Bonjour à tous >>"); 
+	}
+	public  static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix.parler());
+	}
 }
